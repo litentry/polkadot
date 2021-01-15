49eccbd300b61890de6f1e127088e798a55ec2cd
zongxiong.chen <czxczf@gmail.com>
2021-01-15 03:05:44 +0100

    workable version

---
+++ start-alice.sh
@@ -0,0 +1,6 @@
+./target/release/polkadot \
+    --chain rococo-local-cfde-real-overseer.json \
+    --tmp \
+    --ws-port 9944 \
+    --port 30333 \
+    --alice
---
+++ start-bob.sh
@@ -0,0 +1,6 @@
+./target/release/polkadot \
+    --chain rococo-local-cfde-real-overseer.json \
+    --tmp \
+    --ws-port 9955 \
+    --port 30334 \
+    --bob
